9c4a336f702774bec984f486f3de248637dea0c7
Sandesh J Ghimire <sandeshghimire@outlook.com>
2014-06-30 16:26:25 -0700

    adding pid support

--- src/BoardConnetServer.cpp
+++ src/BoardConnetServer.cpp
@@ -1,7 +1,37 @@
 #include "BoardConnetServer.h"
 #include <string>
+#include <syslog.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <sys/wait.h>   /* For WEXITSTATUS */
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <pthread.h>
+#include <syslog.h>
+#include <sched.h>
+#include <mqueue.h>
 
 using namespace std;
+
+#define LOG_EMERGENCY   0
+#define LOG_ALERT   1
+#define LOG_CRITICAL    2
+#define LOG_ERROR   3
+#define LOG_WARNING 4
+#define LOG_NOTICE  5
+#define LOG_INFO 6
+#define LOG_DEBUG 7
 
 namespace BoardConnet
 {
@@ -65,10 +95,19 @@ namespace BoardConnet
     int BoardConnectServer::daemon_init()
     {
         pid_t pid;
+        int rVlaue;
 
-        if((!bci.debug) || ( !bci.foreground))
+        if ((!bci.debug) || (!bci.foreground))
         {
-            if ( 0 < (pid = fork()));
+            if (0 < (pid = fork()))
+                ;
+            {
+                syslog(LOG_DEBUG, "Unable to start daemon");
+                return -1;
+            }
+            (void)setsid();
+            rVlaue = chdir("/");
+            (void)umask(0);
 
         }
         return 0;
